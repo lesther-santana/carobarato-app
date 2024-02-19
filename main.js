b5b0b0b64892333cb79379d6de3d1d669a2b189c
Christopher Santana <christopher@millionairepub.com>
2024-02-18 21:08:39 -0400

    Added file writting for faster testing

--- main.js
+++ main.js
@@ -1,13 +1,44 @@
 import { buildCatalog } from './scraping.js';
+import fs from 'fs';
+import path from 'path';
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const __filename = new URL(import.meta.url).pathname;
+const __dirname = path.dirname(__filename);
+
+const writeFile = (supermarket = 'Nacional', report) => {
+
+    const filepath = path.join(__dirname, 'files', supermarket + '.json');
+
+    fs.writeFile(filepath, JSON.stringify(report), error => {
+
+        if (error) {
+            console.error(`Error Writting file for: ${supermarket}`, error)
+            return;
+        }
+
+        console.log('Wrote file for ', supermarket)
+
+    })
+
+}
 
 async function main() {
     const root_url = "https://supermercadosnacional.com/"
     console.log("Getting html from", root_url)
     console.time('execTime'); // Start timing with a label
     try {
-        const catalog =  await buildCatalog(root_url)
+        const catalog = await buildCatalog(root_url)
         console.log("Done!")
         report(catalog)
+
+
+
+        if (process.env.ENV !== 'prod')
+            writeFile('Nacional', catalog);
+
     } catch (error) {
         console.log(error)
     }

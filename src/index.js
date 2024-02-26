f7a7b07dcbab116086d8eeb095702cb40c3bb903
Christopher Santana <christopher.alesan@gmail.com>
2024-02-25 23:49:10 -0400

    Added server
    Signed-off-by: Christopher Santana <christopher.alesan@gmail.com>

---
+++ src/index.js
@@ -0,0 +1,76 @@
+import net from 'net';
+import express from 'express';
+import dotenv from 'dotenv';
+import router from './routes/index.js';
+import cors from 'cors';
+import os from 'os';
+
+dotenv.config();
+
+// import sequelize from '../database/index.js';
+
+const app = express();
+
+let port = process.env.PORT || 4000;
+
+
+
+const findAvailablePort = (port) => {
+    const server = net.createServer()
+    return new Promise((resolve, reject) => {
+        server.on('error', (err) => {
+            if (err.code === 'EADDRINUSE') {
+                resolve(findAvailablePort(port + 1))
+            } else {
+                reject(err)
+            }
+        })
+        server.listen(port, () => {
+            server.close(() => {
+                resolve(port)
+            })
+        })
+    })
+}
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+
+const corsOptions = {
+    origin: '*', // Replace with your frontend origin
+    optionsSuccessStatus: 200, // Some legacy browsers (IE11, various SmartTVs) choke on 204
+};
+
+app.use(cors(corsOptions))
+
+app.use(router);
+
+
+
+// Usage
+findAvailablePort(port)
+    .then((availablePort) => {
+
+        const hostname = os.hostname();
+
+        console.log(`Available port: ${availablePort}`)
+
+        app.listen(availablePort, () => console.log(`App listening on http://${hostname}:${availablePort}/api`));
+
+        // Use the availablePort for starting the server
+    })
+    .catch((err) => {
+        console.error('Error finding available port:', err)
+    })
+
+
+// sequelize.authenticate()
+//     .then(() => {
+//         console.log('Connection to the database has been established successfully.');
+//         app.listen(port, () => console.log(`App listening on port 4000`));
+//     })
+//     .catch(err => {
+//         console.error('Unable to connect to the database:', err);
+//     });
+
---
+++ src/routes/index.js
@@ -0,0 +1,11 @@
+import express from 'express';
+import { getProducts } from '../controllers/index.js';
+
+const router = express.Router();
+
+
+router.get('/api/products/', getProducts);
+
+
+export default router;
+

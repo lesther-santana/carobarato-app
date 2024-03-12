7e92a5f0a48a21f79a3e2a13153ad4132af57860
Lesther Santana <57154126+lesther-santana@users.noreply.github.com>
2024-03-12 00:57:04 -0400

    Modified Products Query to use full-text search (#10)

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -1,16 +1,28 @@
 import { Product, Price } from "../../database/models/associations.js"
-import { Op } from 'sequelize';
+import { Op, Sequelize } from 'sequelize';
+import format from "pg-format";
 
 export const getProducts = async (req, res) => {
 
     const { search, supermarket } = req.query;
 
+    const ts_search = Sequelize.literal(
+        `product_name_index_col @@ plainto_tsquery('spanish', %L)`, 
+        search
+    )
+
+    const rank = Sequelize.literal(
+        `ts_rank(product_name_index_col, plainto_tsquery('spanish', %L)) DESC`, 
+        search
+    )
+
     const products = await Product.findAll({
         limit: 10,
         where: {
-            product_name: { [Op.iLike]: `%${search}%` },
+            [Op.and]: ts_search,
             supermercado: { [Op.iLike]: `%${supermarket}%` }
         },
+        order: rank,
         include: {
             model: Price,
             as: 'prices'
@@ -20,4 +32,4 @@ export const getProducts = async (req, res) => {
     return res.status(200).json(
         products
     )
-}
+};

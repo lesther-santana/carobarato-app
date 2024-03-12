52fcc453414e84f984f995699fd0272db05a727c
Christopher Santana <christopher.alesan@gmail.com>
2024-03-12 01:35:28 -0400

    Commented the order

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -1,20 +1,20 @@
 import { Product, Price } from "../../database/models/associations.js"
 import { Op, Sequelize } from 'sequelize';
-import format from "pg-format";
 
 export const getProducts = async (req, res) => {
 
     const { search, supermarket } = req.query;
 
     const ts_search = Sequelize.literal(
-        `product_name_index_col @@ plainto_tsquery('spanish', %L)`, 
-        search
+        `product_name_index_col @@ plainto_tsquery('spanish', '${search}')`
+
     )
 
-    const rank = Sequelize.literal(
-        `ts_rank(product_name_index_col, plainto_tsquery('spanish', %L)) DESC`, 
-        search
-    )
+    // const rank = Sequelize.literal(
+    //     `ts_rank(product_name_index_col, plainto_tsquery('spanish', '${search}'))`
+
+    // )
+
 
     const products = await Product.findAll({
         limit: 10,
@@ -22,7 +22,9 @@ export const getProducts = async (req, res) => {
             [Op.and]: ts_search,
             supermercado: { [Op.iLike]: `%${supermarket}%` }
         },
-        order: rank,
+        order: [
+            // [rank, 'DESC']
+        ],
         include: {
             model: Price,
             as: 'prices'

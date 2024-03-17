a0fcefc04ed36cc43f895b14b624957d4d7ab09c
Christopher Santana <christopher.alesan@gmail.com>
2024-03-17 16:25:30 -0400

    Squashed commit of the following:
    commit 61a5ea30eb8b97b5a24b238cd549ea443cd52527
    Author: Christopher Santana <christopher.alesan@gmail.com>
    Date:   Sun Mar 17 16:25:12 2024 -0400
    
        Added new endpoint
    
        Signed-off-by: Christopher Santana <christopher.alesan@gmail.com>
    
    Signed-off-by: Christopher Santana <christopher.alesan@gmail.com>

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -11,6 +11,35 @@ export const getProducts = async (req, res) => {
 
 
     const products = await Product.findAll({
+        limit: 20,
+        where: {
+            [Op.and]: ts_search,
+            supermercado: { [Op.iLike]: `%${supermarket}%` }
+        },
+        order: [
+            // [rank, 'DESC']
+        ],
+        include: {
+            model: Price,
+            as: 'prices'
+        }
+    });
+
+    return res.status(200).json(
+        products
+    )
+};
+export const getSpecificProduct = async (req, res) => {
+
+    const { supermarket } = req.query;
+    const { product } = req.params;
+
+    const ts_search = Sequelize.literal(
+        `product_name_index_col @@ plainto_tsquery('spanish', '${product}')`
+
+    )
+
+    const products = await Product.findOne({
         limit: 10,
         where: {
             [Op.and]: ts_search,
@@ -29,3 +58,61 @@ export const getProducts = async (req, res) => {
         products
     )
 };
+export const getSpecificProductsArray = async (req, res) => {
+
+    const { search } = req.body;
+
+    const products = await Promise.all(search.map(async (p) => {
+        const ts_search = Sequelize.literal(
+            `product_name_index_col @@ plainto_tsquery('spanish', '${p.product}')`
+        )
+        const result = await Product.findOne({
+            where: {
+                [Op.and]: ts_search,
+                supermercado: { [Op.iLike]: `%${p.supermarket}%` }
+            },
+            order: [
+                // [rank, 'DESC']
+            ],
+            include: {
+                model: Price,
+                as: 'prices'
+            }
+        });
+
+        console.log(result.product_name)
+
+        return result;
+    }))
+    return res.status(200).json(products)
+};
+
+// Camaron
+// Sirena
+// CAMARON CRUDO WALA 21/25
+// Nacional
+// Cola De Camarón Crudo 16/20 Congelada, Lb
+// Jumbo
+// Camarón Crudo 16/20 Líder 1 Lb
+// PriceSmart
+// Vima Camarón Crudo Congelado 21-25 Bolsa 1 kg / 2.2 lb
+
+
+// Platanos
+// PriceSmart
+// Member's Selection Plátano Verde 8 Unidades
+// Sirena
+// PLATANO VERDE UND
+// Jumbo
+// Plátano Verde
+// Nacional
+// Platano Verde
+
+
+// Romo
+// Sirena
+// RON XV RESERVA BRUGAL 700 ML
+// Nacional / Jumbo
+// Ron Xv Brugal 70 Cl
+// PriceSmart
+// Brugal Ron Reserva Especial XV en Botella de 700 ml
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,10 +1,12 @@
 import express from 'express';
-import { getProducts } from '../controllers/index.js';
+import { getProducts, getSpecificProduct, getSpecificProductsArray } from '../controllers/index.js';
 
 const router = express.Router();
 
 
 router.get('/api/products/', getProducts);
+router.get('/api/products/:product', getSpecificProduct);
+router.post('/api/specific-products/', getSpecificProductsArray);
 
 
 export default router;
